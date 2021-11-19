4ae3e870cb5b14134a1a02f456c5f459dfe92add
“Cal-107” <kaly___7@hotmail.it>
2021-11-19 16:00:34 +0100

    add push in array and print

--- js/script.js
+++ js/script.js
@@ -5,21 +5,25 @@ const app = new Vue ({
         urlAPI: 'https://flynn.boolean.careers/exercises/api/random/mail',
     },
     created() {
-        this.getAPIMail()
+        this.getAPIMail();
     },
     methods: {
         getAPIMail() {
             // request HTTP --> get
 
             // call the API for
-            axios.get(this.urlAPI)
-            .then ( response => {
-                console.log(response.data.response);
-                response.data.response;
-            })
-            .catch( error => {
-                console.log(error);
-            })
+            for (let i = 0; i < 10; i++) {
+                axios.get(this.urlAPI)
+                .then ( response => {
+                    console.log(response.data.response);
+                    response.data.response;
+                    this.mailsArray.push(response.data.response)
+                })
+                .catch( error => {
+                    console.log(error);
+                })
+            }
         },
     }
 })
+
